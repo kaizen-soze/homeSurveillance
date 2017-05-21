3170056177dfa3983630c961793d3d987b296210
KaizenSoze <kaizensoze42@gmail.com>
2017-05-21 17:46:36 -0400

    Destroying window after each change

--- display.py
+++ display.py
@@ -19,18 +19,18 @@ while(True):
 
     # Display the resulting frame
     cv2.imshow('frame',color)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
     if cv2.waitKey(1):
-    	if  ord('n'):
-	    	current_flag = flags[index]
-	    	print("Current flag: {}".format(current_flag))
-	    	flag = getattr(cv2, flags[index])
-	    	color = cv2.cvtColor(frame, flag)
-	    	cv2.destroyAllWindows()
-	    	cv2.imshow('frame',color)
-	    	index += 1
+    	if ord('q'):
+    		break
+
+    	if ord('n'):
+			current_flag = flags[index]
+			print("Current flag: {}".format(current_flag))
+			flag = getattr(cv2, flags[index])
+			color = cv2.cvtColor(frame, flag)
+			cv2.destroyAllWindows()
+			index += 1
 
 # When everything done, release the capture
 cap.release()
