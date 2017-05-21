7a13b377648fb1a41009b628d415ba5db05b000a
KaizenSoze <kaizensoze42@gmail.com>
2017-05-21 18:08:35 -0400

    Flags should impact display now

--- display.py
+++ display.py
@@ -8,14 +8,15 @@ cap = cv2.VideoCapture(0)
 # Define color flags
 flags = [i for i in dir(cv2) if i.startswith('COLOR_')]
 index = 29
-current_flag = flags[index]
+current_flag = 'COLOR_BGR2HSV'
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    color = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    flag = getattr(cv2, flags[index])
+    color = cv2.cvtColor(frame, flag)
 
     # Display the resulting frame
     cv2.imshow('frame',color)
@@ -24,12 +25,9 @@ while(True):
         break
     
     if pressed == 110:
+        index += 1
         current_flag = flags[index]
         print("Current flag: {}".format(current_flag))
-        flag = getattr(cv2, flags[index])
-        color = cv2.cvtColor(frame, flag)
-        cv2.destroyAllWindows()
-        index += 1
 
 # When everything done, release the capture
 cap.release()
