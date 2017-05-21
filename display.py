4443b24f70563089aa37f5a56296fe0fee7aafec
KaizenSoze <kaizensoze42@gmail.com>
2017-05-21 16:15:03 -0400

    Added camera index switching

--- display.py
+++ display.py
@@ -3,7 +3,8 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(1)
+camera_index = 0
+cap = cv2.VideoCapture(camera_index)
 
 while(True):
     # Capture frame-by-frame
@@ -16,6 +17,12 @@ while(True):
     cv2.imshow('frame',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    elif cv2.waitKey(1) & 0xFF == ord('n'):
+    	camera_index += 1
+    	cap = cv2.VideoCapture(camera_index)
+    	if(!cap.isOpened):
+    		print("CameraIndex {0} failed", format(camera_index))
+    		cap = cv2.VideoCapture(0)
 
 # When everything done, release the capture
 cap.release()
