3faa6c80e943965413d8191e0d3015e702024c3b
KaizenSoze <kaizensoze42@gmail.com>
2017-05-21 18:46:00 -0400

    Trying to write with display script

--- record.py
+++ record.py
@@ -23,14 +23,14 @@ out = cv2.VideoWriter(filename, fourcc, 24.0, (1920,1080))
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret==True:
-        color = cv2.flip(frame,0)
+        frame = cv2.flip(frame,0)
 
         if(cfg['display_window']):
             color = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
             cv2.imshow('frame',color)
 
         # write the flipped frame
-        out.write(color)
+        out.write(frame)
 
         current_time = time.time()
         if (int(current_time) - start_time >= int(cfg['length'])):
