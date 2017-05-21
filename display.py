2f385ddd41aec072c80ca11b8198d4ebbd6eb815
KaizenSoze <kaizensoze42@gmail.com>
2017-05-21 16:59:32 -0400

    Now to attempt color

--- display.py
+++ display.py
@@ -3,26 +3,19 @@
 import numpy as np
 import cv2
 
-camera_index = 0
-cap = cv2.VideoCapture(camera_index)
+cap = cv2.VideoCapture(0)
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    color = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame',color)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    elif cv2.waitKey(1) & 0xFF == ord('n'):
-    	camera_index += 1
-    	cap = cv2.VideoCapture(camera_index)
-    	if(!cap.isOpened):
-    		print("CameraIndex {0} failed", format(camera_index))
-    		cap = cv2.VideoCapture(0)
 
 # When everything done, release the capture
 cap.release()
